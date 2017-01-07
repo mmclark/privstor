2aadae216f8895f152d000891d9f6c319af2ba35
Miles Clark <mmc@obispo.com>
2017-01-07 10:38:00 -0800

    Basic meeting model; use LA timezone

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Meeting
 
-# Register your models here.
+
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'meeting_date', 'start_time')
+
+admin.site.register(Meeting, MeetingAdmin)
+    
--- meetings/models.py
+++ meetings/models.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Meeting(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    meeting_date = models.DateField()
+    start_time = models.TimeField()
+    description = models.TextField(blank=True)
+    
 
     
